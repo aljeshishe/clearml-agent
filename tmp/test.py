45b949e911503c4a78ef654db937b42d1c11ccdf
Alexey Grachev <ax66@bk.ru>
2021-08-19 14:42:28 +0300

    added test

--- tmp/test.py
+++ tmp/test.py
@@ -1,9 +1,23 @@
+import itertools
+
 import clearml
+import plotly.figure_factory as ff
+
+
+def plot(tasks):
+    df = [dict(Task=task.name, Start=task.data.started, Finish=task.data.completed, Resource=task.status)
+          for task in tasks]
+
+    fig = ff.create_gantt(df,index_col='Resource', show_colorbar=True,
+                          group_tasks=True)
+    fig.show()
+
+
 task = clearml.Task.init(project_name='grachev', task_name='Simple clearml task')
 if clearml.config.running_remotely():
     print('waiting')
     import time
-    time.sleep(60)
+    time.sleep(30)
     exit(0)
 else:
     print('Adding tasks')
@@ -31,5 +45,23 @@ else:
     task5.name = '_1gpu'
     clearml.Task.enqueue(task=task5, queue_name='_1gpu')
 
-    print(task1.started, task2.started, task3.started, task4.started, task5.started)
-    print(task1.completed, task2.completed, task3.completed, task4.completed, task5.completed)
+    tasks = [task1, task2, task3, task4, task5]
+    for task in tasks:
+        Status = clearml.Task.TaskStatusEnum
+        task.wait_for_status(status=(Status.completed,),
+                             raise_on_status=(Status.stopped, Status.closed, Status.failed), check_interval_sec=5)
+        data = task.data
+        print(task.name, data.started, data.completed)
+        task.reload()
+
+    plot(tasks)
+
+    sorted_times = [task1.data.started,
+                   task2.data.started, task1.data.completed,
+                   task3.data.started, task2.data.completed,
+                   task4.data.started, task3.data.completed,
+                   task4.data.completed]
+
+    if sorted(sorted_times) != sorted_times:
+        print(f'Times should be in incremetned order:\n{sorted_times}')
+
